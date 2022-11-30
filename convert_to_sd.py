82e10b2ea8fabd0ed4565d65702de75322debe77
RcINS <rcins@duck.com>
2022-12-01 03:02:40 +0800

    convert_to_sd.py add safetensors support

--- convert_to_sd.py
+++ convert_to_sd.py
@@ -7,6 +7,17 @@ from modules.convert.common import DTYPE_CHOICES, STATE_DICT, get_module_state_d
 from modules.convert.diffusers_to_sd import convert_unet_state_dict, convert_vae_state_dict
 
 
+def infer_format_from_path(path: Path):
+    suffix = path.suffix[1:].lower()
+
+    if suffix == "ckpt":
+        return "pt"
+    elif suffix == "safetensors":
+        return "safetensors"
+
+    return None
+
+
 @click.command()
 @click.argument("checkpoint", type=click.File("rb"))
 @click.argument("output", type=click.Path())
@@ -25,14 +36,31 @@ from modules.convert.diffusers_to_sd import convert_unet_state_dict, convert_vae
               default="fp32",
               help="Save text encoder weights in data type.")
 @click.option("--overwrite", is_flag=True)
-@click.option("--map-location", type=str, default="cpu", help='Where the checkpoint is loaded to. Could be "cpu" or "cuda".')
-def main(checkpoint, output, text_encoder, vae, unet_dtype, vae_dtype, text_encoder_dtype, overwrite, map_location):
+@click.option("--map-location", type=str, default="cpu",
+              help='Where the checkpoint is loaded to. Could be "cpu" or "cuda".')
+@click.option("--format",
+              type=click.Choice(["pt", "safetensors"]),
+              default=None,
+              help='Save in which format. If not specified, infered from output path extension.')
+def main(checkpoint, output,
+         text_encoder, vae,
+         unet_dtype, vae_dtype, text_encoder_dtype,
+         overwrite, map_location, format):
     """Converts SCAL-SDT checkpoint to Stable Diffusion format.
 
     CHECKPOINT: Path to the SCAL-SDT checkpoint.
-    OUTPUT: Output Stable Diffusion ckpt path."""
-    if Path(output).exists() and not overwrite:
+    OUTPUT: Output Stable Diffusion checkpoint path."""
+    output = Path(output)
+
+    if output.exists() and not overwrite:
         raise FileExistsError(f"{output} already exists")
+
+    if format is None:
+        infered_format = infer_format_from_path(output)
+        if infered_format is None:
+            raise "Must specify a known extension or format"
+
+        format = infered_format
 
     state_dict: STATE_DICT = torch.load(checkpoint, map_location=map_location)["state_dict"]
 
@@ -56,10 +84,21 @@ def main(checkpoint, output, text_encoder, vae, unet_dtype, vae_dtype, text_enco
 
     # Put together new checkpoint
     state_dict = {**unet_dict, **vae_dict, **text_encoder_dict}
-    ckpt = {"state_dict": state_dict}
 
-    with open(output, 'wb') as f:
-        torch.save(ckpt, f)
+    if format == "pt":
+        with open(output, 'wb') as f:
+            root = {"state_dict": state_dict}
+            torch.save(root, f)
+    elif format == "safetensors":
+        try:
+            from safetensors.torch import save_file
+        except ImportError:
+            raise 'In order to use safetensors, run "pip install safetensors"'
+
+        root = {"state_dict": {k: v.contiguous().to_dense() for k, v in state_dict.items()}}
+        save_file(root, output)
+    else:
+        raise 'Invalid format'
 
 
 if __name__ == '__main__':
